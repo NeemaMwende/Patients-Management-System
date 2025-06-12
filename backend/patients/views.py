af74ede34c9f6f87c845182db9577029fad76e05
NeemaMwende <gracepete12@gmail.com>
2025-06-12 03:04:07 +0300

    patient view and url

--- backend/patients/views.py
+++ backend/patients/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .models import Patient
+from .serializers import PatientSerializer
 
-# Create your views here.
+class PatientListCreateView(generics.ListCreateAPIView):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            return Response(
+                {
+                    'message': 'Patient registered successfully',
+                    'data': serializer.data
+                },
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            {
+                'message': 'Registration failed',
+                'errors': serializer.errors
+            },
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+class PatientDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
